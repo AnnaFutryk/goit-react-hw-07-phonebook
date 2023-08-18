2a19e9bbbf8476a766504c66493cdf8e03b1e227
AnnaFutryk <anna.futryk@gmail.com>
2023-08-18 23:19:07 +0300

    Update contactsApi.js

--- src/API/contactsApi.js
+++ src/API/contactsApi.js
@@ -1,18 +1,21 @@
+import axios from 'axios';
 const BASE_URL = 'https://64dd391be64a8525a0f7c06f.mockapi.io';
 
-export const getContacts = async () => {
-  const data = await fetch(`${BASE_URL}/contacts`);
-  return await data.json();
+export const contactAPI = axios.create({
+  baseURL: 'https://642dff8f2b883abc64053db5.mockapi.io/api/v1',
+});
+
+export const fetchContacts = async () => {
+  const { data } = await contactAPI.get(`${BASE_URL}/contacts`);
+  return data;
 };
 
-export const addContact = async data => {
-  const res = await fetch(`${BASE_URL}/contacts`, {
-    body: JSON.stringify(data),
-  });
-  return await res.json();
+export const addContact = async contact => {
+  const { data } = await contactAPI.post(`${BASE_URL}/contacts`, contact);
+  return data;
 };
 
 export const deleteContact = async id => {
-  const res = await fetch(`${BASE_URL}/contacts/${id}`);
-  return await res.json();
+  const { data } = await contactAPI.delete(`${BASE_URL}/contacts/${id}`);
+  return data;
 };
