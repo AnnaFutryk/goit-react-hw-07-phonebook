47cd9c2813edddb9182ca263b8cce9237f37d84e
AnnaFutryk <anna.futryk@gmail.com>
2023-08-18 23:19:10 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-// import { useState, useEffect } from 'react';
-// import { nanoid } from 'nanoid';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { getContacts } from 'redux/selectors';
+import { getContactsThunk } from 'redux/contacts/thunk';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
@@ -9,7 +9,12 @@ import { Section } from './Section/Section';
 import { Head, SpanFirst, SpanSecond } from './Section/Section.styled';
 
 export const App = () => {
-  const contacts = useSelector(getContacts);
+  const contacts = useSelector(getContacts); //отримання контактів зі стору через селекттор
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContactsThunk()); //асинхронно отримуємо контакти
+  }, [dispatch]);
 
   return (
     <>
